b7ae967876c8465fb7a7684c310407d9736626db
manceauJb <manceau.jeanbaptiste@gmail.com>
2023-06-30 08:07:47 +0200

    ChakraUi - wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Rubik } from 'next/font/google';
+import { theme } from '../../theme';
+
+const rubik = Rubik({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-rubik: ${rubik.style.fontFamily};
+          }
+        `}
+      </style>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
