406f520b449eed15df4119e1fcb37634fa802a38
manceauJb <manceau.jeanbaptiste@gmail.com>
2023-06-30 19:09:51 +0200

    ChakraUi - Home Layout

---
+++ src/components/layouts/BaseLayout/Footer.tsx
@@ -0,0 +1,16 @@
+import { Flex, Link, Text } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <Flex as="footer" width="full" justifyContent="center">
+      <Text fontSize="sm">
+        {new Date().getFullYear()} -{' '}
+        <Link href="https://github.com/manceauJb" isExternal rel="noopener noreferrer">
+            @manceauJb
+        </Link>
+      </Text>
+    </Flex>
+  );
+};
+
+export default Footer;
